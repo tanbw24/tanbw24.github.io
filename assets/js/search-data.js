e62d4e286bc9b16abcbde3d8b8d42dff16e87d8d
tanbw24 <59824062+tanbw24@users.noreply.github.com>
2025-07-23 13:27:41 +0000

    Deploying to gh-pages from @ tanbw24/tanbw24.github.io@71325e1d8a6d87e0e79819a1a7707737ab8a14d1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,6 +429,13 @@ ninja.data = [{
           window.open("https://www.researchgate.net/profile/Bowen-Tan-9/", "_blank");
         },
       },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
